ce7ab689ee72af995910f264d13ae97795dd94b3
Mistium <d.a.kimber@icloud.com>
2024-09-24 03:43:25 +0100

    block changes

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -300,6 +300,9 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
     '<block type="control_if_else" id="control_if_else"></block>' +
     '<block type="control_wait_until" id="control_wait_until"></block>' +
     '<block type="control_repeat_until" id="control_repeat_until"></block>' +
+    '<block type="control_incr_counter" id="control_incr_counter"></block>' +
+    '<block type="control_get_counter" id="control_get_counter"></block>' +
+    '<block type="control_clear_counter" id="control_clear_counter"></block>' +
     '<block type="control_stop" id="control_stop"></block>' +
     '<block type="control_start_as_clone" id="control_start_as_clone"></block>' +
     '<block type="control_create_clone_of" id="control_create_clone_of">' +
